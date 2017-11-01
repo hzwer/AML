1e1b57e363d75bf9a9443717568ddf857cd3a90d
Oscar_Ubuntu <cstdio@sina.cn>
2017-11-01 00:57:29 -0700

    a questionable calculator

--- calc.ml
+++ calc.ml
@@ -1,14 +1,65 @@
 open Ast
 open Printf
+exception Invalid_input;;
+
+let printval = function
+  | Int(a) -> printf "%i" a
+  | Vec(a,b) -> printf "(%i,%i)" a b
+  | Binop(e1,op,e2) -> raise Invalid_input;;
+
 let printop = function
   | Add -> printf " +! "
   | Sub -> printf " -! "
   | Mul -> printf " *! "
   | Div -> printf " /! ";;
-  
+
+let vec_vec_calc x1 y1 op x2 y2 =
+  match op with
+  | Add -> Vec(x1+x2,y1+y2)
+  | Sub -> Vec(x1-x2,y1-y2)
+  | Mul -> Int(x1*x2+y1*y2)
+  | Div -> raise Invalid_input;;
+
+let vec_int_calc x1 y1 op a = 
+  match op with
+  | Mul -> Vec(x1*a, y1*a)
+  | Div -> Vec(x1/a, y1/a)
+  | Add -> raise Invalid_input
+  | Sub -> raise Invalid_input;;
+
+let int_int_calc a op b = 
+  match op with
+  | Add -> Int(a+b)
+  | Sub -> Int(a-b)
+  | Mul -> Int(a*b)
+  | Div -> Int(a/b);;
+
+let rec geo_calc = function
+  | Vec(a, b) -> Vec(a, b)
+  | Int(a) -> Int(a)
+  | Binop(e1, op, e2) -> 
+      match e1 with
+      | Vec(a,b) ->  
+          (
+          match e2 with
+          | Vec(c,d) -> vec_vec_calc a b op c d
+          | Int(c) -> vec_int_calc a b op c
+          | Binop(c, q, d) -> geo_calc(Binop(e1, op, geo_calc(e2)))
+          )
+      | Int(a) ->
+          (
+          match e2 with
+          | Vec(c,d) -> vec_int_calc c d op a
+          | Int(c) -> int_int_calc a op c
+          | Binop(c,q,d) -> geo_calc(Binop(e1, op, geo_calc(e2)))
+          )
+      | Binop(a,q,b) ->
+          geo_calc(Binop(geo_calc(a),q,geo_calc(b)));;
+
 let rec print = function
-  | Vec(a, b) -> printf "(%i, %i)" a b               
-  | Int(a) -> printf "(%i)" a
+  | Vec(a, b) -> printf "(%i,%i)" a b 
+  (*| Vec(a,b) -> (a*b)*)
+  | Int(a) -> printf "(%i)" a 
   | Binop(e1, op, e2) ->
      print e1;
      printop op;
@@ -32,7 +83,9 @@ let _ =
     let lexbuf = Lexing.from_channel stdin in
     while true do
       let result = Parser.main Lexer.token lexbuf in
-      print result;
+      let geo_val = geo_calc result in
+      printval(geo_val);
+(*      printf "%i" (calc result);*)
       printf(";");
       print_newline();
     done

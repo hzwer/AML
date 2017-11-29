5809a8c707a0d6f7580888392cb30a8872f8d315
hzwer <598460606@163.com>
2017-11-29 14:44:03 +0800

    ✨ Add geolib

--- lib/header.ml
+++ lib/header.ml
@@ -1,7 +1,8 @@
 // produced by AML
+#include "geolib.h"
 #include <cmath>
 #include <cstdio>
 #include <cstring>
 #include <iostream>
-#include <algorithm>
+#include <algorithm>  
 using namespace std;  

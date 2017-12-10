5dfad340e878910a640c3a5e77bb8394d0154bcb
hzwer <598460606@163.com>
2017-12-10 12:52:57 +0800

    ✅ Update the test code

--- tests/test.py
+++ tests/test.py
@@ -3,12 +3,18 @@ import os
 tests = ["arith", "if", "loop", "variable", "comment", "function", "block", "vector"]
 tests_agent = ["demo1", "demo2"]
 
-for test in tests:
-    print(test + " test")
-    os.system("./main.native tests/aml/" + test + ".aml" + " > tests/cpp/" + test + ".cpp")
-    os.system("diff tests/cpp/" + test + ".cpp " + "tests/std/" + test + ".cpp")
+l = len(tests)
+for i, test in zip(range(l), tests):
+    os.system("./main.native tests/aml/{0}.aml > tests/cpp/{0}.cpp".format(test))
+    if (os.system("diff tests/cpp/{0}.cpp tests/std/{0}.cpp".format(test)) == 0):
+        print("Pass the test {0} ({0} / {1}) ({2})".format(i + 1, l, test))
+    else :
+        print("Fail the test {0} ({0} / {1}) ({2})".format(i + 1, l, test))
 
-for test in tests_agent:
-    print(test + " test")
-    os.system("./main.native tests/aml/" + test + ".aml --agent" + " > tests/cpp/" + test + ".cpp")
-    os.system("diff tests/cpp/" + test + ".cpp " + "tests/std/" + test + ".cpp")
+l = len(tests_agent)    
+for i, test in zip(range(len(tests_agent)), tests_agent):
+    os.system("./main.native tests/aml/{0}.aml --agent > tests/cpp/{0}.cpp".format(test))
+    if (os.system("diff tests/cpp/{0}.cpp tests/std/{0}.cpp".format(test)) == 0):
+        print("Pass the demo {0} ({0} / {1}) ({2})".format(i + 1, l, test))
+    else :
+        print("Fail the demo {0} ({0} / {1}) ({2})".format(i + 1, l, test))    
